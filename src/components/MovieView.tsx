17cb13a40c31a0e3fc2a593ddbfa68b83f628c1a
Aiden McCaughey <a.mccaughey@ulster.ac.uk>
2021-08-27 13:55:36 +0100

    Refactor movie view

--- src/components/MovieView.tsx
+++ src/components/MovieView.tsx
@@ -11,21 +11,27 @@ function MovieView({ movie }: PropTypes) {
   if (movie === undefined) return null;
 
   // display carousel if more than one image otherwise display poster
-  const images =
-    movie.images.backdrops.length > 1 ? (
-      <MovieCarousel posters={movie.images.backdrops}></MovieCarousel>
-    ) : (
-      <img
-        src={posterUrl(movie.poster_path)}
-        className="card-img-top img-fluid"
-        alt="..."></img>
-    );
+  const images = movie.images.backdrops.length > 1  
+    ? <MovieCarousel posters={movie.images.backdrops}></MovieCarousel>  
+    : <img src={posterUrl(movie.poster_path)} className="card-img-top img-fluid" alt="..."></img>
+
+  // create cast spans
+  const cast = movie.credits.cast.slice(0, 4).map((a, i) => 
+    <span key={i}>{a.name} </span>
+  )
+
+  // create crew spans
+  const crew = movie.credits.crew.slice(0,4).map((a,i) =>
+    <span key={i}>{a.name} </span>
+  )
 
   return (
     <div className="card shadow-lg p-3 mb-5">
       <div className="row g-0">
         <div className="col-md-4">
-          <div className="row g-0 mb-4">{images}</div>
+          <div className="row g-0 mb-4">
+            {images}
+          </div>
           <p>{movie.videos.results.length} Videos available</p>
         </div>
 
@@ -40,14 +46,13 @@ function MovieView({ movie }: PropTypes) {
               <span className="fs-6 text-muted">{movie.release_date}</span>
             </h5>
             <h5 className="card-subtitle mb-2 text-muted">
-              {movie.credits.cast.slice(0, 4).map((a, i) => (
-                <span key={i}>{a.name} </span>
-              ))}
+              Cast ({cast})
             </h5>
-            {/* <h6 className="card-subtitle mb-2 text-muted">{ movie.credits.crew.slice(0,4).map((a,i) =><span key={i}>{a.name} </span>)}</h6> */}
+            <h6 className="card-subtitle mb-2 text-muted">
+              Crew ({crew})
+            </h6> 
             <p className="card-text">{movie.overview}</p>
             <p>
-              {" "}
               Runtime{" "}
               <span className="badge rounded-pill bg-primary">
                 {movie.runtime}
